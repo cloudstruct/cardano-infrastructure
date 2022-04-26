fb2ede0334f3a9aea622197e99dbdf440aa4afcb
Jason Witkowski <jwitko1@gmail.com>
2022-04-26 15:00:32 -0400

    Implement s3 bucket handling and modules

--- terraform/locals.aws.tf
+++ terraform/locals.aws.tf
@@ -1,10 +1,9 @@
 locals {
-  aws_default_region = "us-east-2"
-
+  # Network
   aws_vpc_default = { for i, vpc in local.network_vars :
     vpc.name => vpc if try(vpc.enabled, true) &&
     try(vpc.cloud_provider, "aws") == "aws" &&
-    try(vpc.region, "") == ""
+    (try(vpc.region, "") == "" || try(vpc.region, "us-east-2"))
   }
 
   aws_vpc_sa_east1 = { for i, vpc in local.network_vars :
@@ -19,9 +18,25 @@ locals {
     try(vpc.region, "") == "us-east-1"
   }
 
-  aws_vpc_us_east2 = { for i, vpc in local.network_vars :
-    vpc.name => vpc if try(vpc.enabled, true) &&
-    try(vpc.cloud_provider, "aws") == "aws" &&
-    try(vpc.region, "") == "us-east-2"
+  # Object Storage
+  aws_s3_buckets_default = {
+    for idx, bucket in local.objectstorage_vars : 
+      bucket.name => merge(bucket, { "region" = local.aws_default_region }) if try(bucket.region, "") == "" &&
+      try(bucket.cloud_provider, "aws") == "aws" &&
+      (try(vpc.region, "") == "" || try(vpc.region, "us-east-2"))
+  }
+
+  aws_s3_buckets_sa_east1 = {
+    for idx, bucket in local.objectstorage_vars : 
+      bucket.name => bucket if try(bucket.region, "") == "sa-east-1" &&
+      try(bucket.cloud_provider, "aws") == "aws" &&
+      try(vpc.region, "") == "sa-east-1"
+  }
+
+  aws_s3_buckets_us_east1 = {
+    for idx, bucket in local.objectstorage_vars : 
+      bucket.name => bucket if try(bucket.region, "") == "us-east-1" &&
+      try(bucket.cloud_provider, "aws") == "aws" &&
+      try(vpc.region, "") == "us-east-1"
   }
 }
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,6 @@
 locals {
   config       = try(yamldecode(file("../config.yaml")), {})
   network_vars = try(local.config["network"], [])
+  objectstorage_vars = try(local.config["objectstorage"], [])
+
 }
