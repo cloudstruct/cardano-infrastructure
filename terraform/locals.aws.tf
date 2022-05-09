fd3a213935d3f6bee321b42615e605297b734e4a
Jason Witkowski <jwitko1@gmail.com>
2022-05-09 17:23:21 -0400

    feat: Add bootstrap setup for AWS cloud_provider

--- terraform/locals.aws.tf
+++ terraform/locals.aws.tf
@@ -1,4 +1,6 @@
 locals {
+  aws_default_region = "us-east-2"
+
   # Network
   aws_vpc_default = { for i, vpc in local.network_vars :
     vpc.name => vpc if try(vpc.enabled, true) &&
@@ -28,25 +30,28 @@ locals {
   aws_s3_buckets_default = {
     for idx, bucket in local.objectstorage_vars :
     bucket.name => bucket if try(bucket.region, "") == "" &&
+    try(bucket.terraform_state, false) == false &&
     try(bucket.cloud_provider, "aws") == "aws"
   }
 
   aws_s3_buckets_sa_east1 = {
     for idx, bucket in local.objectstorage_vars :
     bucket.name => bucket if try(bucket.region, "") == "sa-east-1" &&
+    try(bucket.terraform_state, false) == false &&
     try(bucket.cloud_provider, "aws") == "aws"
   }
 
   aws_s3_buckets_us_east1 = {
     for idx, bucket in local.objectstorage_vars :
     bucket.name => bucket if try(bucket.region, "") == "us-east-1" &&
+    try(bucket.terraform_state, false) == false &&
     try(bucket.cloud_provider, "aws") == "aws"
   }
 
   aws_s3_buckets_us_east2 = {
     for idx, bucket in local.objectstorage_vars :
     bucket.name => bucket if try(bucket.region, "") == "us-east-2" &&
+    try(bucket.terraform_state, false) == false &&
     try(bucket.cloud_provider, "aws") == "aws"
   }
-
 }
