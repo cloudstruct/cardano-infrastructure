a1cd08ffe9b193063a4b4cd657838a8276ef5e17
Chris Gianelloni <cgianelloni@cloudstruct.net>
2022-12-21 15:40:06 -0500

    feat: tag ipaddress and ebs with names
    Signed-off-by: Chris Gianelloni <cgianelloni@cloudstruct.net>

--- runme.sh
+++ runme.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 if test -f terraform/backend.tf; then
-  pushd "${SCRIPT_DIR}/terraform" && terraform init && terraform plan
+  pushd "${SCRIPT_DIR}/terraform" && terraform init && terraform ${@:-plan}
   popd
 else
   bash "${SCRIPT_DIR}/scripts/setup-backend.aws.sh"
--- terraform/ipaddress.aws.tf
+++ terraform/ipaddress.aws.tf
@@ -2,22 +2,46 @@ resource "aws_eip" "default" {
   provider = aws
   for_each = local.aws_eip_default
   vpc      = true
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "${each.key}"
+    },
+  )
 }
 
 resource "aws_eip" "sa_east1" {
   provider = aws.sa-east-1
   for_each = local.aws_eip_sa_east1
   vpc      = true
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "${each.key}"
+    },
+  )
 }
 
 resource "aws_eip" "us_east1" {
   provider = aws.us-east-1
   for_each = local.aws_eip_us_east1
   vpc      = true
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "${each.key}"
+    },
+  )
 }
 
 resource "aws_eip" "us_east2" {
   provider = aws.us-east-2
   for_each = local.aws_eip_us_east2
   vpc      = true
+  tags = merge(
+    local.default_tags,
+    {
+      Name = "${each.key}"
+    },
+  )
 }
